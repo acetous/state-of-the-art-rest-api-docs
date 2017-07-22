f909b1182f5f5a085388851bb724a91091f72614
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2017-07-22 15:40:47 +0200

    add spring-restdocs

---
+++ spring-restdocs/src/main/java/de/acetous/examples/restapidocs/springrestdocs/SpringRestdocsApplication.java
@@ -0,0 +1,12 @@
+package de.acetous.examples.restapidocs.springrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestdocsApplication.class, args);
+	}
+}
